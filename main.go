20b343da48d56ac7351cec13165cfc8d336b89cf
David Wilde <d.wilde@selligence.com>
2023-09-18 20:23:39 +0100

    More messing about

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -27,9 +26,10 @@ func main() {
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		// use a template to render the search results
-		t := template.Must(template.ParseFiles("index.html"))
+		t := template.Must(template.ParseFiles("base.html", "index.html"))
 
 		data := []Person{
+			{Name: "Peter", LatestEvent: Event{Title: "Peter's Event", EventType: 1}},
 			{
 				Name: "John",
 				LatestEvent: Event{
@@ -47,22 +47,26 @@ func main() {
 		}
 
 		eventTypeString := r.URL.Query().Get("eventType")
-		subset := []Person{}
-		eventType, err := strconv.Atoi(eventTypeString)
+        action := r.URL.Query().Get("action")
 
-		if err != nil {
-			subset = data
-		} else {
+        if action == "save-as-smart-search" {
+            // redirect to /smart-search but with the query string
+            http.Redirect(w, r, "/smart-search?eventType=" + eventTypeString, 302)
+            return
+        }
+
+		subset := []Person{}
+		eventType, _ := strconv.Atoi(eventTypeString)
+
 			for _, p := range data {
 				if p.LatestEvent.EventType == eventType {
 					subset = append(subset, p)
 				}
 			}
-		}
 
 		results := Results{
 			People:     subset,
-			TotalCount: len(subset),
+            TotalCount: len(subset),
 		}
 
 		if r.Header.Get("Hx-Target") == "results" {
@@ -72,7 +76,30 @@ func main() {
 
 		t.Execute(w, results)
 	}
+
+    h2 := func(w http.ResponseWriter, r *http.Request) {
+        // use a template to render the search results
+        t := template.Must(template.ParseFiles("person.html"))
+        t.Execute(w, nil)
+    }
+
+    h3 := func(w http.ResponseWriter, r *http.Request) {
+        // use a template to render the smart search form
+        t := template.Must(template.ParseFiles("base.html", "smart-search.html"))
+		if r.Header.Get("Hx-Target") == "fullscreen-overlay" {
+            t.ExecuteTemplate(w, "fullscreen-overlay", nil)
+			return
+		}
+        t.Execute(w, nil)
+    }
+
+	h4 := func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.ParseFiles("base.html", "smart-search.html"))
+
+
 	http.HandleFunc("/", h1)
+    http.HandleFunc("/person", h2)
+    http.HandleFunc("/smart-search", h3)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
